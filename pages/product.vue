d3cedefdaf95adb17b78d07416a790a34867c6fa
view-village <Naphat.J@monkeytech.co.th>
2025-06-17 14:48:57 +0700

    test call api

--- pages/product.vue
+++ pages/product.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="container">
-    <h1>Products Page</h1>
-    <p>This is product</p>
-  </div>
+  <div v-if="pending">Loading...</div>
+  <div v-else-if="error">Error: {{ error.message }}</div>
+  <ul>
+    <li v-for="p in data" :key="p.id">{{ p.name }}</li>
+  </ul>
 </template>
+
+
+<script setup>
+const { data, error } = await useFetch('https://localhost:44316/Hitop/Getproduct'
+,{
+  server: false, 
+  watch: [], }
+);
+</script>
